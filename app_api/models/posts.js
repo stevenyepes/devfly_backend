f4222d4a2cf57a6233268a09c066f86fc0b11b32
Steven <jsteven.yepes@gmail.com>
2016-06-29 11:01:21 -0500

    added pug instead of deprecated jade

--- app_api/models/posts.js
+++ app_api/models/posts.js
@@ -10,13 +10,14 @@ var reviewSchema = new mongoose.Schema({
 
 var PostSchema = new mongoose.Schema({
   title: {type: String, required: true},
-  keywords: {type:[String], required: true},
+  keywords: {type: [String], required: true},
   author: {type: String, required: true},
-  content: {type:String, required: true},
+  content: {type: String, required: true},
   rating: {type: Number, "default": 0, min: 0, max:5},
   date: {type:Date, "default": Date.now},
   views: Number,
   image: { data: Buffer, contentType: String },
+  category: {type: String, required: true},
   reviews : [reviewSchema]
 });
 
