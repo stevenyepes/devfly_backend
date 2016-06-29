c31f7765bd3b525109d7ae076226b541be750311
Steven <jsteven.yepes@gmail.com>
2016-06-29 15:42:27 -0500

    brach production

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -4,5 +4,5 @@ module.exports.homelist = function(req, res) {
     /* Render the view index and the second argument is
      * An object, it can be a Json object or an array
      * Try to change the title or add some other information */
-    res.render('index', {title: "DevFly"});
+    res.render('index', {title: "DevFly-production"});
 };
