8331abb5494acc1ca71d872af09ffd07bd60053a
Steven <jsteven.yepes@gmail.com>
2016-06-29 21:26:42 -0500

    added gulp task to wacth scss and nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 
 var gulp = require('gulp');
-
+var nodemon = require('gulp-nodemon');
 var sass = require('gulp-sass');
+
 gulp.task('sass', function () {
   gulp.src('./public/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -11,3 +12,17 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./public/sass/**/*.scss', ['sass']);
 });
+
+// start our server and listen for changes
+gulp.task('server', function() {
+    // configure nodemon
+    nodemon({
+        // the script to run the app
+        script: './bin/www',
+        // this listens to changes in any of these files/routes and restarts the application
+        ext: 'js jade'
+        
+    });
+});
+
+gulp.task('default', ['server', 'sass:watch']);
