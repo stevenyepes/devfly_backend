f2e2f1b5eb6148340f71141b6b6ee216378aac5e
Steven <jsteven.yepes@gmail.com>
2016-07-01 12:02:13 -0500

    addedsome tests

--- spec/api/posts_test_spec.js
+++ spec/api/posts_test_spec.js
@@ -1,6 +1,7 @@
 var frisby = require('frisby');
-var url = 'http://localhost:3000/api/v1/posts';
+//var url = 'http://localhost:3000/api/v1/posts';
 
+var url = 'http://devflybldr-devfly.rhcloud.com/api/v1/posts';
 frisby.create('Ensure response has proper JSON types in specified keys')
   .get(url)
   .expectStatus(200)
@@ -13,7 +14,7 @@ frisby.create('Ensure response has proper JSON types in specified keys')
   })
 .toss();
 
-frisby.create('Ensure response has proper JSON types in specified keys')
+/*frisby.create('Ensure response has proper JSON types in specified keys')
   .addHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Nzc1OWE1YzYzZWFkOTJhMThhNDQ4MGEiLCJlbWFpbCI6InN0ZXZlbkBnbWFpbC5jb20iLCJuYW1lIjoic3RldmVuIiwiZXhwIjoxNDY3OTMzNjA5LCJpYXQiOjE0NjczMjg4MDl9.4_aYljdDXPKfT26vd8ZRe1L2Sig9UinKDsyLwuCUke8')
   .post(url,{
     title: "testing unauthorize user",
@@ -25,3 +26,4 @@ frisby.create('Ensure response has proper JSON types in specified keys')
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
 .toss();
+*/
