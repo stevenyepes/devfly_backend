5253b28c8d798fb9933d648b8e4be5ac213f323f
Steven <jsteven.yepes@gmail.com>
2016-07-11 21:23:57 -0500

    fix some bug with sharp junk

--- app_api/utils.js
+++ app_api/utils.js
@@ -1,6 +1,6 @@
 var fileType = require('file-type');
-var sharp = require('sharp');
-var Jimp = require('jimp')
+var Jimp = require('jimp');
+
 sendJSONResponse = function(res, status, content) {
   res.status(status);
   res.json(content);
@@ -8,25 +8,6 @@ sendJSONResponse = function(res, status, content) {
 
 resizeImage = function(res,options, done) {
 
-  /*sharp(options.path)
-    .resize(options.width, options.height)
-    .background({r: 0, g: 0, b: 0, a: 0})
-    .embed()
-    .toFormat(sharp.format.webp)
-    .toBuffer(function(err, outputBuffer) {
-      if (err) {
-        sendJSONResponse(res, 404, {
-          message: "Can't open the image, be sure to send a valid type of image"
-        });
-        return;
-      }
-      // outputBuffer contains WebP image data of a 512 pixels wide and 512 pixels high
-      // containing a scaled version, embedded on a transparent canvas, of input image
-      // send the buffer and mimetype
-      var contentType = fileType(outputBuffer).mime;
-      done(outputBuffer, contentType);
-
-    });*/
     Jimp.read(options.path, function (err, image) {
       if (err) {
         sendJSONResponse(res, 404, {
