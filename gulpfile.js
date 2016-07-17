205063714b661a78d2016b68d31cad4d88f83de7
Steven <jsteven.yepes@gmail.com>
2016-07-16 19:12:55 -0500

    gulp task for pug added

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var sass = require('gulp-sass');
+var pug = require('gulp-pug');
 
 gulp.task('sass', function () {
   gulp.src('./public/sass/**/*.scss')
@@ -13,6 +14,18 @@ gulp.task('sass:watch', function () {
   gulp.watch('./public/sass/**/*.scss', ['sass']);
 });
 
+gulp.task('views', function buildHTML() {
+  return gulp.src('./public/app_client/pug/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./public/app_client/views'));
+});
+
+gulp.task('pug:watch', function () {
+  gulp.watch('./public/app_client/pug/*.pug', ['views']);
+});
+
 // start our server and listen for changes
 gulp.task('server', function() {
     // configure nodemon
@@ -21,8 +34,8 @@ gulp.task('server', function() {
         script: './bin/www',
         // this listens to changes in any of these files/routes and restarts the application
         ext: 'js jade'
-        
+
     });
 });
 
-gulp.task('default', ['server', 'sass:watch']);
+gulp.task('default', ['server', 'sass:watch', 'pug:watch']);
